aaed2a69f13a3c6b25b7d8e306a4103d87ece24e
jufusong <jufusong@gmail.com>
2015-12-12 18:03:06 +0800

    18:03

--- app1/views.py
+++ app1/views.py
@@ -6,11 +6,20 @@ import json
 # Create your views here.
 
 def index (request):
-    return render_to_response ('app1/index.html', {},
+    menu = ['o1', 'o2', 'o3']
+    return render_to_response ('app1/predicttoday.html', {'menu':menu},
                                context_instance = RequestContext (request))
 
 def getOrderData (request):
     response_data = {}
-    response_data['categories'] = ['111', '222', '333', '444', '555', '666']
-    response_data['values'] = [5, 6, 3, 4, 7, 22]
+    response_data['categories'] = []
+    response_data['values'] = []
+    for x in range(48):
+        response_data['categories'].append (5)
+        response_data['values'].append (6)
+    print (response_data['categories'])
+  
+
+#    response_data['categories'] = ['111', '222', '333', '444', '555', '666']
+#    response_data['values'] = [5, 6, 3, 4, 7, 22]
     return HttpResponse(json.dumps(response_data), content_type="application/json")
