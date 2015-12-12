1fe684678f25d58a7d4920adf26e6fafaff3e7d8
jufusong <jufusong@gmail.com>
2015-12-13 04:32:14 +0800

    04:32

--- predictorder/views.py
+++ predictorder/views.py
@@ -20,8 +20,18 @@ def index (request):
                                context_instance = RequestContext (request))
 
 def predictOrder (request):
-    menu = ['o1', 'o2', 'o3']
-    return render_to_response ('app1/predicttoday.html', {'menu':menu},
+    shopid = request.GET.get ('shopid', 256365)
+    dashid = request.GET.get ('dashid')
+    request.session['shopid'] = shopid
+    menu = getMenu (shopid)
+    nowdash = ""
+    if 'dashid' in request.GET:
+        nowdash = request.GET['dashid']
+    else:
+        for k, v in menu.items ():
+            nowdash = k
+            break
+    return render_to_response ('app1/predicttoday.html', {'menu':menu, 'dashid':nowdash},
                                context_instance = RequestContext (request))
 
 def statisticOrder (request):
@@ -40,12 +50,37 @@ def statisticOrder (request):
                                context_instance = RequestContext (request))
 
 def getOrderData (request):
+    shopid = request.session.get ('shopid', 256365)
+    nowdash = int(request.GET['dashid'])
+    menu = getMenu (shopid)
+    now = nnow()
+    before = (now - datetime.timedelta(days=7))
+    res = compviews.QueryRestaurantOrders(shopid,before,now)
+    data = collections.OrderedDict()
+    timeStamp = datetime.datetime.strptime("00:00",'%H:%M')
+    for i in range (48):
+        t = (timeStamp + datetime.timedelta(minutes=30*i)).strftime("%H:%M")
+        data[t] = 0
+    for x in res:
+        timeStamp = datetime.datetime.strptime(x['created_at'],'%Y-%m-%d %H:%M:%S')
+        if timeStamp.strftime('%m%d') == before.strftime('%m%d'):
+            t = timeStamp.strftime('%H') + ':'
+            if int(timeStamp.strftime('%M')) >= 30:
+                t += '30'
+            else:
+                t += '00'
+            group = x['detail']['group']
+            for va in group:
+                for v in va:
+                    if v['id'] == nowdash:
+                        data[t] = data.get (t, 0) + 1
+
     response_data = {}
     response_data['categories'] = []
     response_data['values'] = []
-    for x in range(48):
-        response_data['categories'].append (5)
-        response_data['values'].append (6)
+    for k, v in data.items():
+        response_data['categories'].append (k)
+        response_data['values'].append (v)
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def getStatisticData (request):
