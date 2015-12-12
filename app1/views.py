508f4191b6803e356603def658ccbc87a6a71ba7
jufusong <jufusong@gmail.com>
2015-12-12 16:31:57 +0800

    1631

--- app1/views.py
+++ app1/views.py
@@ -12,5 +12,5 @@ def index (request):
 def getOrderData (request):
     response_data = {}
     response_data['categories'] = ['111', '222', '333', '444', '555', '666']
-    response_data['values'] = [5, 6, 3, 4, 7]
+    response_data['values'] = [5, 6, 3, 4, 7, 22]
     return HttpResponse(json.dumps(response_data), content_type="application/json")
